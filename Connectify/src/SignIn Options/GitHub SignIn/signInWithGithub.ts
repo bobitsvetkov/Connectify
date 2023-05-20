38002ecfa45f73286ec071e101cd9bd335bf6b36
Borislav <borislav.tsvetkov02@gmail.com>
2023-05-20 20:39:33 +0300

    add: add additional options for user to sign up/sign in
    the additional options are: Google, Facebook and Github

---
+++ Connectify/src/SignIn Options/GitHub SignIn/signInWithGithub.ts
@@ -0,0 +1,22 @@
+import { signInWithPopup } from "firebase/auth";
+import { ref, set } from 'firebase/database';
+import { database, githubProvider } from "../../config/firebaseConfig";
+import { auth } from "../../common/constants";
+export const signInWithGithub = async () => {
+    try {
+        const result = await signInWithPopup(auth, githubProvider);
+        const user = result.user;
+        if (user) {
+            const userData = {
+                uid: user.uid,
+                displayName: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                phoneNumber: user.phoneNumber,
+            };
+            await set(ref(database, `users/${user.uid}`), userData);
+        }
+    } catch (error) {
+        console.error("Error signing in with Github: ", error);
+    }
+}
