f6fdccb1de9965a0f096e31134c700940d1e7bee
Borislav <borislav.tsvetkov02@gmail.com>
2023-05-20 19:28:30 +0300

    add: add firebase config

---
+++ Connectify/src/config/firebaseConfig.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider } from 'firebase/auth'
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAPGia1tPS4LXgVC_fI_mpxHAkN_-1JD6c",
+    authDomain: "connectify-7b76f.firebaseapp.com",
+    databaseURL: "https://connectify-7b76f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "connectify-7b76f",
+    storageBucket: "connectify-7b76f.appspot.com",
+    messagingSenderId: "1054986592214",
+    appId: "1:1054986592214:web:e4f49d60c3a9b0654a8e77",
+    measurementId: "G-9FRX3VZV9R"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
+export const githubProvider = new GithubAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
