ec19d80c1685eaf04a8a7ef5c0ab5b343ca8aa5e
Radina Georgieva <radina940729@abv.bg>
2023-05-21 22:17:30 +0300

    add:adding HomePage with Sidebar and Header

---
+++ Connectify/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/HeaderMenu/HeaderMenu";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default HomePage;
