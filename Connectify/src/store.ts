27b51e1d5b45d2c8e8c3f2ddbeb1578604baf7e8
Viktor Lomliev <viktor.lomliev@gmail.com>
2023-05-24 13:30:34 +0300

    add: teams api and add it to the store

--- Connectify/src/store.ts
+++ Connectify/src/store.ts
@@ -2,17 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersApi } from "./api/UsersApi";
 import { chatsApi } from "./api/ChatsApi"; // import chatsApi
 import activeUserReducer from "./features/ActiveUserSlice";
+import { teamsApi } from "./api/TeamsApi";
 
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
     [chatsApi.reducerPath]: chatsApi.reducer,
+    [teamsApi.reducerPath]: teamsApi.reducer,
     activeUser: activeUserReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(usersApi.middleware)
-      .concat(chatsApi.middleware), 
+      .concat(chatsApi.middleware)
+      .concat(teamsApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
