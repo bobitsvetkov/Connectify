ada5c8ed0faffd10c780fd0e1d5e3f8c1aa10b14
Radina Georgieva <radina940729@abv.bg>
2023-05-25 11:43:35 +0300

    add :add additional  functionality in Userlist
    close the userlist after click on the user

--- Connectify/src/components/UserList.tsx
+++ Connectify/src/components/UserList.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { useGetUsersQuery } from "../api/UsersApi";
 import { selectUser } from "../features/ActiveUserSlice";
 import { useDispatch } from "react-redux";
-import { User } from '../api/UsersApi';
+import { User } from "../api/UsersApi";
 
 const UserList = () => {
   const { data: users, isLoading, isError } = useGetUsersQuery();
   const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleUserClick = (user: User) => {
+    dispatch(selectUser(user));
+    setIsOpen(false);
+  };
+
+  const handleTransitionEnd = () => {
+    if (!isOpen) {
+      setIsOpen(true);
+    }
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -14,21 +27,28 @@ const UserList = () => {
   if (isError || !users) {
     return <div>Error loading users</div>;
   }
-  
-  const handleUserClick = (user: User) => {
-    dispatch(selectUser(user));
-  };
 
   return (
-    <div>
-      <h2>Users</h2>
-      <ul>
-        {Object.values(users).map((user: User, index: number) => (
-          <li key={index} onClick={() => handleUserClick(user)} style={{ cursor: 'pointer'}} >
-            {user.email}
-          </li>
-        ))}
-      </ul>
+    <div
+      className={`user-list ${isOpen ? "open" : "closed"}`}
+      onTransitionEnd={handleTransitionEnd}
+    >
+      {isOpen && (
+        <>
+          <h2>Users</h2>
+          <ul>
+            {Object.values(users).map((user: User, index: number) => (
+              <li
+                key={index}
+                onClick={() => handleUserClick(user)}
+                style={{ cursor: "pointer" }}
+              >
+                {user.email}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
