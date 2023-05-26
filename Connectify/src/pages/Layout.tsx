518f43840ba5705868f88020a4558fd333987854
Radina Georgieva <radina940729@abv.bg>
2023-05-27 00:01:46 +0300

    change:changing the components in Layout

--- Connectify/src/pages/Layout.tsx
+++ Connectify/src/pages/Layout.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { Flex, Box } from "@chakra-ui/react";
-import Header from "../components/HeaderMenu/HeaderMenu";
-import Sidebar from "../components/Sidebar/Sidebar";
-import ChatBox from "../components/ChatBox/ChatBox";
-import { Divider } from "@chakra-ui/react";
-import FooterDetails from "../components/Footer";
-
-function Layout({ children }) {
+import { LeftPanel } from "../components/LeftPanel";
+import RightPanel from "../components/RightPanel";
+function Layout() {
   return (
-    <Flex direction="column" height="100vh" overflow="hidden">
-      <Header />
-      <Flex direction="row" flex="1" overflow="hidden">
-        <Sidebar pr={4} />
-        <Divider orientation="vertical" />
-        <Box flex="1" overflow="hidden">
-          {children}
-        </Box>
-      </Flex>
-      <FooterDetails />
+    <Flex h="100vh">
+      <LeftPanel />
+      <RightPanel />
     </Flex>
   );
 }
