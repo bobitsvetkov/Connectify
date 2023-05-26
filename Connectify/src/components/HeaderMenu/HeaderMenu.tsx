9eb2227fc513800ee5ba0243b8987b8f3c3e9d38
Radina Georgieva <radina940729@abv.bg>
2023-05-26 23:50:24 +0300

    change Header design
    changing the whole concept of the Header

--- Connectify/src/components/HeaderMenu/HeaderMenu.tsx
+++ Connectify/src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,30 +1,144 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+import {
+  Avatar,
+  Flex,
+  HStack,
+  IconButton,
+  Tooltip,
+  useDisclosure,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+import { IoIosPeople } from "react-icons/io";
+import { GrStatusGoodSmall } from "react-icons/gr";
+import { BsFillChatLeftTextFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { UserSetting } from "../UserSettings/UserSettings";
 import AvatarButton from "../AvatarItem/AvatarButton";
-import SearchInput from "../Search/SearchInput";
+import ProfileInfo from "../ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const Header: React.FC = () => {
+interface IconData {
+  icon: ReactNode;
+  label: string;
+}
+
+interface CustomTooltipProps extends React.ComponentProps<typeof Tooltip> {
+  label: string;
+  icon: ReactNode;
+}
+
+export const Header: React.FC = ({
+  onViewChange,
+  onChatClick,
+  setUserListOpen,
+}) => {
+  const {
+    isOpen: isAvatarOpen,
+    onOpen: onAvatarOpen,
+    onClose: onAvatarClose,
+  } = useDisclosure();
+  const {
+    isOpen: isCommunityChatOpen,
+    onOpen: onCommunityChatOpen,
+    onClose: onCommunityChatClose,
+  } = useDisclosure();
+  const {
+    isOpen: isSettingsOpen,
+    onOpen: onSettingsOpen,
+    onClose: onSettingsClose,
+  } = useDisclosure();
+
+  const handleChatClick = () => {
+    onChatClick();
+    onViewChange("chat");
+    setUserListOpen(true);
+  };
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    navigate("/");
+  };
+
   return (
-    <Box bg="green.800" color="white" px={4}>
-      <Flex
-        align="center"
-        justify="space-between"
-        maxW="1200px"
-        mx="auto"
-        py={4}
-        position="relative"
-      >
-        <Text fontSize="xl" fontWeight="bold">
-          Connectify
-        </Text>
-        <Flex align="center">
-          <SearchInput size="sm" />
-        </Flex>
-        <Flex>
-          <AvatarButton />
-        </Flex>
-      </Flex>
-    </Box>
+    <Flex
+      bg="#f0f2f5"
+      justify="space-between"
+      py="2"
+      px="4"
+      borderRight="1px solid #f2f2f2"
+      color="#54656f"
+    >
+      <AvatarButton onClick={onAvatarOpen} boxSize="40px" />
+      <Drawer placement="left" onClose={onAvatarClose} isOpen={isAvatarOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Profile</DrawerHeader>
+          <DrawerBody>
+            <ProfileInfo />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+
+      <HStack spacing="3">
+        <IconButton
+          variant="ghost"
+          onClick={onCommunityChatOpen}
+          icon={<IoIosPeople />}
+        />
+        <Drawer
+          placement="left"
+          onClose={onCommunityChatClose}
+          isOpen={isCommunityChatOpen}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerHeader borderBottomWidth="1px">Community Chat</DrawerHeader>
+            <DrawerBody>{/* content */}</DrawerBody>
+          </DrawerContent>
+        </Drawer>
+        <IconButton
+          variant="ghost"
+          onClick={handleChatClick}
+          icon={<BsFillChatLeftTextFill />}
+        />
+
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            variant="ghost"
+            icon={<GiHamburgerMenu />}
+          />
+          <MenuList>
+            <MenuItem onClick={onSettingsOpen}>Settings</MenuItem>
+            <Drawer
+              placement="left"
+              onClose={onSettingsClose}
+              isOpen={isSettingsOpen}
+            >
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerHeader borderBottomWidth="1px">
+                  <UserSetting />
+                </DrawerHeader>
+                <DrawerBody>{/* content */}</DrawerBody>
+              </DrawerContent>
+            </Drawer>
+            <MenuItem>New Window</MenuItem>
+            <MenuDivider />
+            <MenuItem onClick={handleLogOut}>Log out</MenuItem>
+          </MenuList>
+        </Menu>
+      </HStack>
+    </Flex>
   );
 };
-
-export default Header;
