bab7bd45e94df4b15aeffa95ff247da6e8ee3c3e
Radina Georgieva <radina940729@abv.bg>
2023-05-29 23:55:43 +0300

    add:adding new reducers setStatus in usersSlice

--- Connectify/src/features/UsersSlice.ts
+++ Connectify/src/features/UsersSlice.ts
@@ -1,16 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { User } from '../types/interfaces';
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../types/interfaces";
 
 const usersSlice = createSlice({
-  name: 'users',
+  name: "users",
   initialState: [] as User[],
   reducers: {
     setUsers: (state, action) => {
       return action.payload;
-    }
+    },
+    setStatus: (
+      state,
+      action: PayloadAction<{ uid: string; status: string }>
+    ) => {
+      const { uid, status } = action.payload;
+      const user = state.find((user) => user.uid === uid);
+      if (user) {
+        user.status = status;
+      }
+    },
   },
 });
- 
-export const { setUsers } = usersSlice.actions;
+
+export const { setUsers, setStatus } = usersSlice.actions;
 export default usersSlice.reducer;
