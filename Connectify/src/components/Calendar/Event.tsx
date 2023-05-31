10929addab4ff4e1adf59a7720f3271510b8a7a3
Radina Georgieva <radina940729@abv.bg>
2023-05-31 19:34:09 +0300

    add:adding EventComponent to display the event in the calendar
    has an option to delete the event

---
+++ Connectify/src/components/Calendar/Event.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { Event } from "../../types/interfaces";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+
+interface EventComponentProps {
+  event: Event;
+  onDelete: () => void;
+}
+
+const EventComponent: React.FC<EventComponentProps> = ({ event, onDelete }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleDelete = () => {
+    onDelete();
+    toggleModal();
+  };
+  const isPast = new Date(event.end) < new Date();
+
+  return (
+    <div style={{ textDecoration: isPast ? "line-through" : "none" }}>
+      <div onClick={toggleModal}>{event.title}</div>
+      <Modal isOpen={isModalOpen} onClose={toggleModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Event Details</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div>
+              <Text>Title:</Text> {event.title}
+            </div>
+            <div>
+              <Text>Start:</Text> {event.start.toString()}
+            </div>
+            <div>
+              <Text>End:</Text> {event.end.toString()}
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="red" onClick={handleDelete}>
+              Delete
+            </Button>
+            <Button variant="ghost" onClick={toggleModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+
+export default EventComponent;
