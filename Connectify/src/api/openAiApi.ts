455020d59da79275d4ed0127c2335626ec9d18f7
Viktor Lomliev <viktor.lomliev@gmail.com>
2023-06-02 15:18:25 +0300

    add: change ai model and api endpoint

--- Connectify/src/api/openAiApi.ts
+++ Connectify/src/api/openAiApi.ts
@@ -1,27 +1,33 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const openaiApi = createApi({
+export const openaiApi = createApi({
     reducerPath: 'openaiApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://api.openai.com/v1',
         prepareHeaders: (headers) => {
-            headers.set('Authorization', `Bearer "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"`);
+            headers.set('Authorization', "Bearer " + "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");
             headers.set('Content-Type', 'application/json');
             return headers;
         },
     }),
     endpoints: (builder) => ({
-        generateMessage: builder.query({
-            query: (message) => ({
-                url: 'engines/davinci-codex/completions',
+        generateConversation: builder.query({
+            query: (messages) => ({
+                url: '/chat/completions',
                 method: 'POST',
                 body: {
-                    prompt: message,
-                    max_tokens: 60,
+                    model: 'gpt-3.5-turbo',
+                    messages: messages,
+                    temperature: 0.7,
+                    max_tokens: 150,
+                    top_p: 1,
+                    frequency_penalty: 0.0,
+                    presence_penalty: 0.6,
                 },
             }),
         }),
     }),
 });
 
-export const { useGenerateMessageQuery } = openaiApi;
+export const { useGenerateConversationQuery } = openaiApi;
+
