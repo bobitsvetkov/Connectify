cf106816fbb2d4319383f734ca97bf4ee2363bbb
Viktor Lomliev <viktor.lomliev@gmail.com>
2023-06-02 19:16:09 +0300

    refactor: removed unused code, and add types of the props

--- Connectify/src/components/ChatInput/ChatInput.tsx
+++ Connectify/src/components/ChatInput/ChatInput.tsx
@@ -1,24 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Input, Button, HStack, useToast } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
-import { useAddMessageToChatMutation, useAddMessageToChannelMutation, useUpdateUserLatestChatsMutation, useGetTeamByIdQuery } from "../../api/databaseApi";
+import { useAddMessageToChatMutation, useAddMessageToChannelMutation, useUpdateUserLatestChatsMutation, useGetTeamByIdQuery, User } from "../../api/databaseApi";
 import Emojis from "../ChatBox/Emojis/Emojis";
 import useVoiceMessages from "../../Hooks/useVoiceMessages";
 import { useLazyGenerateConversationQuery } from "../../api/openAiApi";
+interface ChatInputProps {
+  currUser: object,
+  user: User,
+  chatUserId: string,
+  activeChatUser: User,
+  isChat: boolean,
+  teamId: string,
+  channelId: string,
+  isBot: boolean,
+}
 
-const ChatInput = ({ currUser, user, chatUserId, activeChatUser, isChat, teamId, channelId, isBot }) => {
+const ChatInput: React.FC<ChatInputProps> = ({ currUser, user, chatUserId, activeChatUser, isChat, teamId, channelId, isBot }) => {
   const [message, setMessage] = useState<string>("");
   const [emojiPickerState, SetEmojiPickerState] = useState<boolean>(false);
   const [messagesForAI, setMessagesForAI] = useState<Array<{ role: string, content: string }>>([{ "role": "system", "content": "You are a helpful assistant." }]);
 
-  const [addMessageToChat, { isLoading: isAddingMessage }] = useAddMessageToChatMutation();
-  const [addMessageToChannel, { isLoading: isAddingMessageToChannel }] = useAddMessageToChannelMutation();
+  const [addMessageToChat] = useAddMessageToChatMutation();
+  const [addMessageToChannel] = useAddMessageToChannelMutation();
   const toast = useToast();
-  const [updateLatestChats, { isLoading: isUpdatingLatestChats }] = useUpdateUserLatestChatsMutation();
-  const { data: team } = useGetTeamByIdQuery(teamId) || null;
+  const [updateLatestChats] = useUpdateUserLatestChatsMutation();
+  const { data: } = useGetTeamByIdQuery(teamId) || null;
   const { recording, handleStart, handleSendAudio } = useVoiceMessages(currUser, user, chatUserId, isChat, teamId, channelId, addMessageToChat, addMessageToChannel, toast);
-  const [executeGenerateConversation, { data: generatedMessage, error }] = useLazyGenerateConversationQuery();
-  console.log(generatedMessage);
+  const [executeGenerateConversation] = useLazyGenerateConversationQuery();
+
   const userIds = [chatUserId, user.username];
   userIds.sort();
   const chatId = userIds.join("-");
@@ -30,34 +40,35 @@ const ChatInput = ({ currUser, user, chatUserId, activeChatUser, isChat, teamId,
       content: message,
       date: new Date().toISOString(),
     };
-  
+
     if (message.trim().length > 0 && currUser && user) {
       if (isChat) {
         updateLatestChats({ userUid: currUser.uid, chatUid: chatId, message: { ...newMessage, isChat: isChat, userChatting: activeChatUser.uid, userChattingUsername: chatUserId } });
         updateLatestChats({ userUid: activeChatUser.uid, chatUid: chatId, message: { ...newMessage, isChat: isChat, userChatting: currUser.uid, userChattingUsername: user.username } });
         addMessageToChat({ chatId: chatId, message: newMessage });
       } else {
+        
         updateLatestChats({ userUid: currUser.uid, chatUid: channelId, message: { ...newMessage, isChat: isChat, teamId: teamId, channelId: channelId } });
         Object.entries(team.participants).map(([userUid, isMember]) => {
           updateLatestChats({ userUid: userUid, chatUid: channelId, message: { ...newMessage, isChat: isChat, teamId: teamId, channelId: channelId } });
         })
         addMessageToChannel({ teamId: teamId, channelId: channelId, message: newMessage });
       }
-  
+
       setMessage("");
     }
-  
+
     if (isBot) {
       const updatedMessagesForAI = [
         ...messagesForAI,
         { role: 'user', content: message }
       ];
-  
+
       setMessagesForAI(updatedMessagesForAI);
-  
+
       try {
         const generatedMessage = await executeGenerateConversation(updatedMessagesForAI);
-  
+
         if (generatedMessage.data) {
           const aiMessage = {
             uid: uuidv4(),
@@ -66,11 +77,17 @@ const ChatInput = ({ currUser, user, chatUserId, activeChatUser, isChat, teamId,
             date: new Date().toISOString(),
             isGenerated: true,
           };
-    
+
           addMessageToChat({ chatId: chatId, message: aiMessage });
         }
       } catch (error) {
-        console.error(error);
+        toast({
+          title: "An error occurred.",
+          description: error.message,
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
       }
     }
   };
@@ -97,32 +114,20 @@ const ChatInput = ({ currUser, user, chatUserId, activeChatUser, isChat, teamId,
       {message ? (
         <Button
           onClick={handleSend}
-          isLoading={isAddingMessage || isAddingMessageToChannel}
           colorScheme="teal"
         >
           Send
         </Button>
       ) : (
-        !recording ? (
-          <Button
-            onClick={handleStart}
-            colorScheme="teal"
-          >
-            Record
-          </Button>
-        ) : (
-          <>
-            <Button
-              onClick={handleSendAudio}
-              colorScheme="blue"
-            >
-              Send Audio
-            </Button>
-          </>
-        )
+        <Button
+          onClick={!recording ? handleStart : handleSendAudio}
+          colorScheme={!recording ? "teal" : "blue"}
+        >
+          {!recording ? "Record" : "Send Audio"}
+        </Button>
       )}
     </HStack>
-  )
-}
+  );
+};
 
 export default ChatInput;
