1db0e546750ceefa2527e9369442d08a74f9002f
Viktor Lomliev <viktor.lomliev@gmail.com>
2023-06-03 14:38:37 +0300

    add: agora io setup and modify the create room component

--- Connectify/src/components/Video Call/CreateRoom.tsx
+++ Connectify/src/components/Video Call/CreateRoom.tsx
@@ -1,51 +1,24 @@
-import { useState } from "react";
-import { Button, Spacer } from "@chakra-ui/react";
-import { FaPhone, FaVideo } from "react-icons/fa";
-import VideoCall from "./VideoCall";
+import {useState} from 'react';
+import AgoraUIKit from 'agora-react-uikit';
 
 const CreateRoom = () => {
-  const [isVideoCall, setIsVideoCall] = useState(true);
-  const [callUrl, setCallUrl] = useState(null);
-
-  const createCall = async (isVideoEnabled) => {
-    try {
-      const response = await fetch("https://api.daily.co/v1/rooms", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${"e25a6624f282752a924fde861e4fb4110cbada0efe6e758445b816a9adb23a44"}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          properties: { enable_screenshare: isVideoEnabled, enable_chat: true },
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const room = await response.json();
-      setCallUrl(room.url);
-      setIsVideoCall(isVideoEnabled);
-      // TODO: Share this URL with the other user.
-    } catch (err) {
-      console.error(err);
-    }
+  const [videoCall, setVideoCall] = useState(true);
+  const rtcProps = {
+    appId: '1d9b4d5e8c224e6895d2904de8db2e39', 
+    channel: 'test', // your agora channel
+    token: null // use null or skip if using app in testing mode
+  };
+  const callbacks = {
+    EndCall: () => setVideoCall(false),
   };
 
-  const createAudioCall = () => {
-    createCall(false);
-  };
-
-  const createVideoCall = () => {
-    createCall(true);
-  };
-
-  return (
-    <>
-      <Button leftIcon={<FaVideo />} onClick={createVideoCall}></Button>
-      {callUrl && <VideoCall callUrl={callUrl} isVideoCall={isVideoCall} />}
-    </>
+  
+  return videoCall ? (
+    <div style={{display: 'flex', width: '100vw', height: '100vh'}}>
+      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+    </div>
+  ) : (
+    <h3 onClick={() => setVideoCall(true)}>Start Call</h3>
   );
 };
 
