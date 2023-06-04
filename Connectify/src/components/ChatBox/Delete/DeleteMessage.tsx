0d299bd2f3c6ff0720259850eef8b751549357fc
Borislav <borislav.tsvetkov02@gmail.com>
2023-06-04 23:21:00 +0300

    fix: decouple remove call so there will be different call for audio and image files

--- Connectify/src/components/ChatBox/Delete/DeleteMessage.tsx
+++ Connectify/src/components/ChatBox/Delete/DeleteMessage.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
-import {
-    useRemoveMessageFromChatMutation,
-} from "../../../api/databaseApi";
+import { deleteObject, ref } from "firebase/storage";
+import { storage } from "../../../config/firebaseConfig";
+import { useRemoveMessageFromChatMutation } from "../../../api/databaseApi";
 import {
     Button,
     Modal,
     ModalOverlay,
     ModalContent,
     ModalHeader,
-    ModalBody,
     ModalCloseButton,
+    ModalBody,
+    ModalFooter,
 } from "@chakra-ui/react";
+import { Message } from "../../../api/databaseApi";
+import { useRef } from 'react';
 
 type RemoveMessageParams = {
     chatId: string;
@@ -24,16 +27,39 @@ type DeleteMessageProps = {
     teamId?: string;
     isDeleting: boolean;
     setIsDeleting: (isDeleting: boolean) => void;
+    message: Message;
 };
 
-function DeleteMessage({ chatId, messageId, teamId, isDeleting, setIsDeleting }: DeleteMessageProps) {
+function DeleteMessage({ chatId, messageId, teamId, isDeleting, setIsDeleting, message, }: DeleteMessageProps) {
     const [removeMessage] = useRemoveMessageFromChatMutation();
+    const cancelRef = useRef();
 
-    const handleRemoveMessage = () => {
+    const handleRemoveMessage = async () => {
+        // delete the message from the database
         const params: RemoveMessageParams = { chatId, messageId, teamId };
         removeMessage(params)
             .unwrap()
-            .then(() => setIsDeleting(false))
+            .then(() => {
+                let storagePath;
+                // if message type is image or audio, delete it from Firebase storage
+                if (message.type === 'image') {
+                    storagePath = `images/${message.fileName}`;
+                } else if (message.type === 'audio') {
+                    storagePath = `audio/${message.fileName}`;
+                }
+
+                if (storagePath) {
+                    const storageRef = ref(storage, storagePath);
+                    deleteObject(storageRef).catch((error) => {
+                        if (error.code === 'storage/object-not-found') {
+                            console.error("The file does not exist in storage. Ignoring error.");
+                        } else {
+                            console.error("Failed to remove file from storage: ", error);
+                        }
+                    });
+                }
+                setIsDeleting(false);
+            })
             .catch((error) => {
                 console.error("Failed to remove message: ", error);
             });
@@ -47,14 +73,15 @@ function DeleteMessage({ chatId, messageId, teamId, isDeleting, setIsDeleting }:
                     <ModalHeader>Delete Message</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <p>Are you sure you want to delete this message?</p>
-                        <Button colorScheme="red" onClick={handleRemoveMessage} mt={4}>
+                        Are you sure you want to delete this message? This action cannot be undone.
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button onClick={() => setIsDeleting(false)}>Cancel</Button>
+                        <Button colorScheme="red" onClick={handleRemoveMessage} ml={3}>
                             Delete
                         </Button>
-                        <Button variant="ghost" onClick={() => setIsDeleting(false)} mt={4}>
-                            Cancel
-                        </Button>
-                    </ModalBody>
+                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
