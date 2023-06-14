9ce421cd56e6d75307aec1ae801a246d348add0d
Borislav <borislav.tsvetkov02@gmail.com>
2023-06-14 17:44:04 +0300

    fix: fix type errors in video view

--- Connectify/src/components/Video Call/VideoCall.tsx
+++ Connectify/src/components/Video Call/VideoCall.tsx
@@ -1,51 +1,51 @@
-import React, { useEffect, useRef } from 'react';
-import DailyIframe from '@daily-co/daily-js';
-import { useState } from 'react';
+// import React, { useEffect, useRef } from 'react';
+// import DailyIframe from '@daily-co/daily-js';
+// import { useState } from 'react';
 
-const VideoCall = ({ callUrl, isVideoCall }) => {
-    const dailyRef = useRef(null);
-    const [isMounted, setIsMounted] = useState(true);
+// const VideoCall = ({ callUrl, isVideoCall }) => {
+//     const dailyRef = useRef(null);
+//     const [isMounted, setIsMounted] = useState(true);
 
-    useEffect(() => {
-        setIsMounted(true);
-        return () => {
-            setIsMounted(false);
-        };
-    }, []);
+//     useEffect(() => {
+//         setIsMounted(true);
+//         return () => {
+//             setIsMounted(false);
+//         };
+//     }, []);
 
-    useEffect(() => {
-        if (!dailyRef.current && isMounted) {
-            dailyRef.current = DailyIframe.createFrame({
-                showLeaveButton: true,
-                iframeStyle: {
-                    position: 'fixed',
-                    top: '0',
-                    left: '0',
-                    width: '100%',
-                    height: '100%',
-                },
-            });
+//     useEffect(() => {
+//         if (!dailyRef.current && isMounted) {
+//             dailyRef.current = DailyIframe.createFrame({
+//                 showLeaveButton: true,
+//                 iframeStyle: {
+//                     position: 'fixed',
+//                     top: '0',
+//                     left: '0',
+//                     width: '100%',
+//                     height: '100%',
+//                 },
+//             });
 
-            dailyRef.current.join({ url: callUrl }).then(() => {
-                if (!isVideoCall) {
-                    dailyRef.current.setLocalVideo(false);
-                }
-            });
-        }
+//             dailyRef.current.join({ url: callUrl }).then(() => {
+//                 if (!isVideoCall) {
+//                     dailyRef.current.setLocalVideo(false);
+//                 }
+//             });
+//         }
 
-        return () => {
-            if (dailyRef.current) {
-                dailyRef.current.leave().then(() => {
-                    if (isMounted) {
-                        dailyRef.current.destroy();
-                        dailyRef.current = null;
-                    }
-                });
-            }
-        };
-    }, [callUrl, isVideoCall, isMounted]);
+//         return () => {
+//             if (dailyRef.current) {
+//                 dailyRef.current.leave().then(() => {
+//                     if (isMounted) {
+//                         dailyRef.current.destroy();
+//                         dailyRef.current = null;
+//                     }
+//                 });
+//             }
+//         };
+//     }, [callUrl, isVideoCall, isMounted]);
 
-    return null;
-};
+//     return null;
+// };
 
-export default VideoCall;
+// export default VideoCall;
