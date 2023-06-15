a6a5d5a9048906dc005c158f35dc635fb7303113
Radina Georgieva <radina940729@abv.bg>
2023-06-15 13:21:27 +0300

    add:add an Image and make RightPanel responsive

--- Connectify/src/components/RightPanel.tsx
+++ Connectify/src/components/RightPanel.tsx
@@ -8,9 +8,14 @@ import {
   HStack,
   Text,
   useColorModeValue,
+  Image,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { RootState } from "../store";
 import { RightPanelProps } from "../types/interfaces";
+import { motion } from "framer-motion"; // import framer motion
+
+const MotionImage = motion(Image);
 
 export const RightPanel: React.FC<RightPanelProps> = ({ children }) => {
   const activeChatUser = useSelector(
@@ -20,9 +25,10 @@ export const RightPanel: React.FC<RightPanelProps> = ({ children }) => {
   const bgColor = useColorModeValue("gray.200", "gray.700");
   const textColor = useColorModeValue("#41525d", "#f0f2f5");
   const subTextColor = useColorModeValue("#8696a0", "#a0aec0");
+  const fontSize = useBreakpointValue({ base: "xl", md: "3xl" });
 
   return (
-    <Center bg={bgColor} position="relative" w="70%">
+    <Center bg={bgColor} position="relative" w={["100%", "70%"]}>
       {activeChatUser ? (
         <>{children}</>
       ) : (
@@ -31,17 +37,35 @@ export const RightPanel: React.FC<RightPanelProps> = ({ children }) => {
           textAlign="center"
           color={textColor}
           align="center"
+          p="4"
         >
           <Box pt="8">
-            <Heading fontWeight="light">Connectify</Heading>
-            <Text fontSize="sm" mt="4">
-              Send and receive messages without keeping your phone online.{" "}
-              <br /> Use Connectify
-            </Text>
+            <Heading fontSize={fontSize} fontWeight="bold">
+              {" "}
+              Welcome to Connectify
+            </Heading>
+            <Box boxSize={["sm", "lg"]}>
+              <MotionImage
+                borderRadius={"20px"}
+                animate={{ y: ["0px", "10px", "0px"] }}
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                  times: [0, 0.5, 1],
+                  loop: Infinity,
+                  repeatDelay: 1,
+                }}
+                src="https://i.ibb.co/p3sbtWy/20945188-removebg-preview-1.png"
+              />
+              <Text fontSize={["sm", "md"]} mt="2">
+                Bridging Distances, Connecting Minds!
+                <br />
+              </Text>
+            </Box>
           </Box>
           <AbsoluteCenter axis="horizontal" bottom="10" flex="1" mt="10">
             <HStack justifyItems="baseline" color={subTextColor}>
-              <Text fontSize="sm" fontWeight="medium">
+              <Text fontSize={["xs", "sm"]} fontWeight="medium">
                 Supported by Connectify Team
               </Text>
             </HStack>
